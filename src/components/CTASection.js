dd0781b726e68e8697113614cb2402545684a2ac
Ondřej Chrastina <chrastina.ondra@gmail.com>
2019-05-10 13:59:45 +0200

    add cta section

--- src/components/CTASection.js
+++ src/components/CTASection.js
@@ -1,17 +1,27 @@
 import React from 'react';
 
-const ctaSection = () => (
-  <section id="four" class="main style2 special">
-    <div class="container">
-      <header class="major">
-        <h2>Ipsum feugiat consequat?</h2>
-      </header>
-      <p>Sed lacus nascetur ac ante amet sapien.</p>
-      <ul class="actions uniform">
-        <li><a href="#" class="button special">Sign Up</a></li>
-        <li><a href="#" class="button">Learn More</a></li>
-      </ul>
-    </div>
-  </section>
-);
+const ctaSection = ({ elements }) => {
+  const buttons = elements.buttons.map(({ elements }) =>
+    <li>
+      <a
+        href={elements.url.value}
+        class={`button ${elements.special.value.map((isChecked) => isChecked.codename)}`}>
+        {elements.text.value}
+      </a>
+    </li>);
+
+  return (
+    <section id="four" class="main style2 special">
+      <div class="container">
+        <header class="major">
+          <h2>{elements.primary_text.value}</h2>
+        </header>
+        <p>{elements.secondary_text.value}</p>
+        <ul class="actions uniform">
+          {buttons}
+        </ul>
+      </div>
+    </section>
+  );
+}
 export default ctaSection;
