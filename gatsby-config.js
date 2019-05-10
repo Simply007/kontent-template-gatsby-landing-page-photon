717ff330859fd076c29a0a35bb863dbdae3d50c1
Ondřej Chrastina <chrastina.ondra@gmail.com>
2019-05-10 14:28:31 +0200

    images cleanup

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,7 +27,7 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/assets/images/website-icon.png', // This path is relative to the root of the site.
+        icon: 'src/assets/images/kentico-cloud.png', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-sass',
