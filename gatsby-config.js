18e3805fbe1966f4a333ed490df881e42b308832
Ondřej Chrastina <chrastina.ondra@gmail.com>
2019-05-10 16:05:02 +0200

    adjust manifest colors

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,11 +21,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'kentico-cloud-gatsby-template',
+        short_name: 'kc-gatsby',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#0087DC',
+        theme_color: '#0087DC',
         display: 'minimal-ui',
         icon: 'src/assets/images/kentico-cloud.png', // This path is relative to the root of the site.
       },
